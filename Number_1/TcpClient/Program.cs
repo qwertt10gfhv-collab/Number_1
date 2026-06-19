570ef6785adc140ca48d0dad1a2a554deea53e28
qwertt10gfhv-collab <qwertt10gfhv@gmail.com>
2026-06-19 08:40:43 +0300

    Загружен проект

---
+++ Number_1/TcpClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        string name = "Иванов Иван";
+
+        using (TcpClient client = new TcpClient("127.0.0.1", 3361))
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] data = Encoding.UTF8.GetBytes(name);
+            stream.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ Number_1/TcpServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 3361);
+        listener.Start();
+
+        TcpClient client = listener.AcceptTcpClient();
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytes = stream.Read(buffer, 0, buffer.Length);
+        string name = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+        Console.WriteLine("Получено: " + name);
+
+        string reply = "Привет, " + name + "!";
+        byte[] replyData = Encoding.UTF8.GetBytes(reply);
+        stream.Write(replyData, 0, replyData.Length);
+
+        stream.Close();
+        client.Close();
+        listener.Stop();
+    }
+}
